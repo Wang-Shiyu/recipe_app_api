70f7f7a290fa9b2432b20f88adc758ff32b85aac
Wang-Shiyu <wsy_samuel@outlook.com>
2021-10-21 18:00:11 -0500

    Added recipe feature.

--- app/core/models.py
+++ app/core/models.py
@@ -65,3 +65,24 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    # another type of FK
+    # provide name of the class or class
+    # but providing class directly will require thr class to be put in
+    # dependency order
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
